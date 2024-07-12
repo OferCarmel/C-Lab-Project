45616ca5a43584b0d5e93b3dfa0e3b6336da15a4
osher <opappo@gmail.com>
2024-07-12 21:09:03 +0300

    add dec_to_bin function

--- assembler.c
+++ assembler.c
@@ -2,10 +2,9 @@
 #include <stdlib.h>
 
 int dec_to_bin(int, int);
-
 main()
 {
-	dec_to_bin(10, 10);
+	dec_to_bin(-10, 10);
 	return 1;
 	//testing 2 changes 
 }
@@ -13,6 +12,9 @@ main()
 int dec_to_bin(int dec, int size)
 {
 	char* num = (char*)malloc(size * sizeof(char));
+	char* num_start = num;
+	char i = 0;
+ 
 	char negative = 0;
 
 	if (num == NULL) {
@@ -20,20 +22,21 @@ int dec_to_bin(int dec, int size)
 		return -1;  // Return an error code
 	}
 
-	if(num)
+	if(num)   /*insert into num from the end*/
 		num += size - 1;
+	int delta;
 
-	while (num>=0)
-	{
+	while (num >= num_start) {
 		*num = dec & 1;
 		dec = dec >> 1;
 		num--;
 	}
 
-	for (int i = 0; i < size; i++)
-	{
-		printf("%d", *num);
+	// Print the binary number for verification
+	for (int i = 0; i < size; i++) {
+		printf("%d", num_start[i]);
 	}
+	getchar();
 	return 1;
 	// my changes
 }
