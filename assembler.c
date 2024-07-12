3698ac435c7211b5a70aba24e397b299ea5283f5
osher <opappo@gmail.com>
2024-07-12 18:42:50 +0300

    first assembler version

---
+++ assembler.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int dec_to_bin(int, int);
+
+main()
+{
+	dec_to_bin(10, 10);
+	return 1;
+}
+
+int dec_to_bin(int dec, int size)
+{
+	char* num = (char*)malloc(size * sizeof(char));
+	char negative = 0;
+
+	if (num == NULL) {
+		printf("Memory allocation failed\n");
+		return -1;  // Return an error code
+	}
+
+	if(num)
+		num += size - 1;
+
+	while (num>=0)
+	{
+		*num = dec & 1;
+		dec = dec >> 1;
+		num--;
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		/*print("%d", )*/
+	}
+	return 1;
+}
