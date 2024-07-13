ad1b3981e534cc529631642d97ea4295e08db8d4
osher <opappo@gmail.com>
2024-07-13 11:14:13 +0300

    new added

---
+++ typedef.c
@@ -0,0 +1,18 @@
+typedef enum {
+    MOV = 0,
+    CMP,
+    ADD,
+    SUB,
+    LEA,
+    CLR,
+    NOT,
+    INC,
+    DEC,
+    JMP,
+    BNE,
+    RED,
+    PRN,
+    JSR,
+    RTS,
+    STOP
+} OpCode;
